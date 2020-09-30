e53945a428b8ddcf7fc5f4aeb06da676656c0fca
ijeriel <ijeriel@users.noreply.github.com>
2020-09-30 16:31:17 -0700

    Add files via upload

--- Python-challenge/PyBank/main.py
+++ Python-challenge/PyBank/main.py
@@ -7,14 +7,29 @@ import csv
 #define path to csv file
 budget_csv = os.path.join('Resources', 'budget_data.csv')
 
-#Count number of rows
 with open(budget_csv) as csv_file:
     reader = csv.reader(csv_file, delimiter=",")
 
+#Count rows without header
     csv_header = next(csv_file)
     lines = len(list(reader))
 
+#print title for output
+    print("Financial Analysis")
+    print("--------------------------")
 #print out total months
     print(f"Total Months:  {str(lines)}")
 
-#calcualate total profit/loss
+#calcualations
+budget_data = list(budget_csv)
+profloss_sum = 0
+for row in budget_data:
+    profloss = int(row[1])
+
+    print(f"Total:  $ {sum(profloss)}")
+
+#calculate avg change
+
+#output greatest increase in profit
+
+#output greatest decrease in profit
