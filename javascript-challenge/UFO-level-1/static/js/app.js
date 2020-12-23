a063f44450981b059225e3881cb6bfc1540c4b99
Jeriel Tenorio <jeriel.tenorio@Jeriels-MacBook-Pro.local>
2020-12-23 15:41:29 -0800

    add td elements

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -5,7 +5,21 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 // console.log(data);
+
+//log each value in data
 data.forEach(function(ufoReport) {
     console.log(ufoReport);
+
+    //create table row elements
     var row = tbody.append("tr");
+
+    //extract key value pairs
+    Object.entries(ufoReport).forEach(function([key, value]){
+        console.log(key, value);
+
+        //append td element to rows
+        var cell = row.append("td");
+
+    })
+
 })
