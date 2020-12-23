901c8de5864c1374ff3e32fe8656bf2e17afc416
Jeriel Tenorio <jeriel.tenorio@Jeriels-MacBook-Pro.local>
2020-12-23 15:23:22 -0800

    add ufoReport function

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+// console.log(data);
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+})
