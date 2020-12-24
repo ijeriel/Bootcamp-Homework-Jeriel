5ae1f65b25393c30470662c2458f40231634b084
Jeriel Tenorio <jeriel.tenorio@Jeriels-MacBook-Pro.local>
2020-12-23 18:39:58 -0800

    create table data to build function

--- javascript-challenge/UFO-level-1/static/js/app.js
+++ javascript-challenge/UFO-level-1/static/js/app.js
@@ -6,33 +6,58 @@ var tableData = data;
 var tbody = d3.select("tbody");
 // console.log(data);
 
-//log each value in data
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
+function build(data) {
 
-    //create table row elements
-    var row = tbody.append("tr");
-
-    //extract key value pairs
-    Object.entries(ufoReport).forEach(function([key, value]){
-        console.log(key, value);
-
-        //append td element to rows
-        var cell = row.append("td");
-
-        //add data to object
-        cell.text(value)
-
+    tbody.html("");
+    data.forEach(function(ufoReport) {
+        console.log(ufoReport);
+    
+        //create table row elements
+        var row = tbody.append("tr");
+    
+        //extract key value pairs
+        Object.entries(ufoReport).forEach(function([key, value]){
+            console.log(key, value);
+    
+            //append td element to rows
+            var cell = row.append("td");
+    
+            //add data to object
+            cell.text(value)
+    
+        });
+    
     });
+}
+//log each value in data
+// data.forEach(function(ufoReport) {
+//     console.log(ufoReport);
 
-});
+//     //create table row elements
+//     var row = tbody.append("tr");
+
+//     //extract key value pairs
+//     Object.entries(ufoReport).forEach(function([key, value]){
+//         console.log(key, value);
+
+//         //append td element to rows
+//         var cell = row.append("td");
+
+//         //add data to object
+//         cell.text(value)
+
+//     });
+
+// });
 
 //select button
 var button = d3.select("#filter-btn");
+var field = d3.select("#datetime");
 
 //create event handlers
 button.on("click", runEnter);
-form.on("submit", runEnter);
+field.on("submit", runEnter);
+// form.on("submit", runEnter);
 
 function runEnter() {
 
@@ -50,14 +75,6 @@ function runEnter() {
     //use form to filter by date
     var filteredData = tableData.filter(day => day.datetime === inputValue);
     console.log(filteredData);
-
-
-
-
-
-    
-
-
-
-
+    build(filteredData);
 }
+build(data);
