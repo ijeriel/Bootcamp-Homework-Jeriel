a786dac07ca60b302bc511810a8be754b373cdb8
ijeriel <ijeriel@gmail.com>
2021-03-01 00:50:25 -0800

    delete api keys

--- Python-API-challenge/VacationPy/api_keys.py
+++ Python-API-challenge/VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "d451ee13e832d9de51e49951499b83f8"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAWqtFE8zv_-moW8ICOhQnoFn9J-yuSHVc"
+g_key = ""
