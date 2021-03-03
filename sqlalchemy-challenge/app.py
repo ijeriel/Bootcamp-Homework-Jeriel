003163b958899d3872b1ba84ed20476439ac1c31
ijeriel <ijeriel@gmail.com>
2021-03-02 20:46:35 -0800

    fix prcp df

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -1,1 +1,18 @@
 #sqlalchemy-challenge
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("Server recieved request for 'Home' page ...")
+    return "Welcome to my 'Home' page!"
+
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return("Welcome to my 'about' page")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
